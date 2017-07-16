53f2d1a18b23becd6de0e94254b04132a9da6b33
vvksingh24 <vvksingh24@gmail.com>
2017-07-16 16:32:30 +0530

    intial commit

---
+++ prog.py
@@ -0,0 +1,24 @@
+f=dict()
+f[1] = 1
+f[2] = 1
+
+def fib(n):
+    
+    if (n == 0):
+        return 0
+    if (n == 1 or n == 2):
+        return f[n]
+        
+    if (f.get(n, None)):
+        return f[n]
+        
+    k = (n+1)/2 if (n & 1)  else n/2
+    
+    f[n] = (fib(k)*fib(k) + fib(k-1)*fib(k-1)) if (n & 1) else (2*fib(k-1) + fib(k))*fib(k)
+    
+    return f[n]
+t=input()
+while t>0:
+    n=input()
+    print fib(n+2)
+    t-=1
